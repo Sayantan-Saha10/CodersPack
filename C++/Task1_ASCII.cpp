58feb1382862c022ac3257c6095b3dbaa55c1483
Sayantan-Saha10 <52678183+Sayantan-Saha10@users.noreply.github.com>
2019-07-19 13:31:33 +0530

    Uploaded 1 file

---
+++ C++/Task1_ASCII.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int a;
+	char c;
+	cout << "Enter the character: ";
+	cin >> c;
+	a = c;
+	cout << "The ASCII value is " << a << endl;
+	return 0;
+}
